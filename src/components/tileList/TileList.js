e62c5b49c9a64860a0c2508191e9ff6c9bef6d50
Muhammed Shittu <devshittu@gmail.com>
2023-12-01 02:23:32 +0000

    Added the TileList

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Tile} from "../tile/Tile";
 
-export const TileList = () => {
+export function TileList({ items = [] }) {
   return (
     <div>
-      
+      {items.map((item, index) => (
+        <Tile key={index} name={item.name} description={item} />
+      ))}
     </div>
   );
-};
+}
+
+
+// src/components/tileList/TileList.js
