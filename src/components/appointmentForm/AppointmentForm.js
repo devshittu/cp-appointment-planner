58beaa1925be2d79666681b33f97712db5828503
Muhammed Shittu <devshittu@gmail.com>
2023-12-01 02:22:23 +0000

    Added the AppointmentForm

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -21,6 +22,32 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      <ContactPicker
+        contacts={contacts}
+        value={contact}
+        onChange={(e) => setContact(e.target.value)}
+      />
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        min={getTodayString()}
+      />
+      <input
+        type="time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      />
+      <button type="submit">Add Appointment</button>
+    </form>
   );
 };
+
+// src/components/appointmentForm/AppointmentForm.js
